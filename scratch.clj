80a73343cb9a30e78abe7a500567807e4dbfb4e9
brianh <brian.h.work@gmail.com>
2009-03-11 15:29:34 -0600

    Playing around

--- scratch.clj
+++ scratch.clj
@@ -9,6 +9,42 @@
 	     [[:x] [:emp :age] [:emp :address :state]]
 	     [9 20 :NY]))
 
+(defn
+ #^{:arglists '([map key val] [map key val & kvs])
+    :doc "assoc[iate]. When applied to a map, returns a new map of the
+    same (hashed/sorted) type, that contains the mapping of key(s) to
+    val(s). When applied to a vector, returns a new vector that
+    contains val at index. Note - index must be <= (count vector)."}
+ my-assoc
+ ([map key val] (if (coll? key)
+		  (let [curmap (get map (first key))]
+			(assoc-in curmap (rest key) val))
+		  (assoc map key val)))
+ ([map key val & kvs]
+    (let [ret (my-assoc map key val)]
+      (if kvs
+        (recur ret (first kvs) (second kvs) (nnext kvs))
+        ret))))
+
+(reduce get a ks)
+
+(def whole-nums (iterate inc 1))
+
+(defn buildup [coll]
+  (for [n (iterate inc 1) :let [c (count coll)] :while (<= n c)] (take n coll)))
+
+(for [n t] (take-while (complement #{n}) t))
+
+
+(map (partial get a) (for [k ks] (
+
+				  
+(map vec (reduce (fn [m k]
+		   (let [v (get m k)]
+		     (if (coll? v)
+		     v
+		     nil)) a ks)))
+
 (defn accessor [a]
   (cond
     (number? a) (fn [c] (nth c a))
