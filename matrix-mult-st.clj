fe00c079e9591c5ed4247b57dde888c99c61e715
brianh <brian.h.work@gmail.com>
2009-03-10 22:34:14 -0600

    Minor tweaks

--- matrix-mult-st.clj
+++ matrix-mult-st.clj
@@ -1,16 +1,11 @@
 (defn make-matrix
   ( [r c]
-      (make-matrix r c (partial repeat 0)))
-  ( [r c f]
-      {:rows r :cols c :data (into [] (take r (repeatedly #(into [] (map agent (take c (repeatedly f)))))))}))
+      (make-matrix r c (repeat 0)))
+  ( [r c coll]
+      {:rows r :cols c :data (partition c (take (* r c) coll))}))
 
-(defn make-matrix
-  ( [r c]
-      (make-matrix r c (partial repeat 0)))
-  ( [r c f]
-      {:rows r :cols c :data (into [] (take r (repeatedly #(into [] (take c (repeatedly f))))))}))
-
-(partial rand-int 1000)
+(defn rand-ints [max]
+  (repeatedly (partial rand-int max)))
 
 (into [] (map #(let [r (.get % 0)] @r) (:data m1)))
 (vec (map #(let [r (.get % 0)] @r) (:data m1)))
@@ -18,24 +13,28 @@
 (defn get-cols [m]
   (for [c (range (:cols m))]
     (get-col m c)))
-      
-
-			   
+	   
 (defn get-col [m c]
    (map #(let [r (nth % c)] r) (:data m)))
 
-(defn get-row [m r]
-  (nth (:data m) r))
+;(defn get-row [m r]
+ ; (nth (:data m) r))
+
+(defn matrix-mult [m1 m2]
+  {:rows (:rows m1) :cols (:cols m2)
+   :data (vec (partition (:cols m2) (for [r (:data m1) c (get-cols m2)]
+	   (reduce + (map #(* @%1 @%2) r c)))))})
 
 (defn matrix-mult [m1 m2]
   {:rows (:rows m1) :cols (:cols m2)
    :data (vec (partition (:cols m2) (for [r (map @ (:data m1)) c (map @ (get-cols m2))]
 	   (reduce + (map #(* %1 %2) r c)))))})
 
-(defn matrix-mult [m1 m2]
-  {:rows (:rows m1) :cols (:cols m2)
-   :data (vec (partition (:cols m2) (for [r (map @ (:data m1)) c (map @ (get-cols m2))]
-	   (reduce + (map #(* %1 %2) r c)))))})
+(defn m-mult [m1 m2]
+  (make-matrix (:rows m1) (:cols m2)
+	       (for [r (:data m1)
+		     c (get-cols m2)]
+		 (reduce + (map #(* %1 %2) r c)))))
 
 (defn matrix-deref [m]
   (for [r (:data m)
