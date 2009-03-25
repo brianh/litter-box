9dec00c226a5d35ac14a125e729126371a00a1e2
brianh <brian.h.work@gmail.com>
2009-03-25 15:10:47 -0600

    more

--- santaII.clj
+++ santaII.clj
@@ -12,64 +12,89 @@
   (load-file "C:\\home\\lisp\\clj\\src\\git-repos\\litterbox\\santaII.clj")
 )
 
-(def output-lock (ref 0))
-(def screw-off-time 10000) ;in milliseconds
+(declare screw-off)
 
-(defmulti screw-off (fn [s mtg-place] (:Type s)))
-(defmulti queue-up (fn [s mtg-place] (:Type s)))
+(def output-lock (ref 0))
+(def screw-off-time 20000) ;in milliseconds
 
 (defn display [& args]
+  "Ensures write order"
   (let [output (apply str (interpose " " args))]
     (dosync
      (ensure output-lock)
      (println output))))
 
-(defmethod screw-off :elf [s mtg-place]
-  (do
-    (display "Elf" (:id s) "pretending to do something...")
-    (ignore [InterruptedException] (sleep-rnd screw-off-time))
-    (display "Elf" (:id s) "done pretending...")
-    (send-off s queue-up mtg-place)
-    s))
-(defmethod screw-off :deer [s mtg-place]
-  (do
-    (display "Deer" (:id s) "off on holiday...")
-    (ignore [InterruptedException] (sleep-rnd screw-off-time))
-    (send-off s queue-up mtg-place)
-    s))
+(defstruct task :delay :start-txt :fun :args)
+;(defstruct task :delay :start-txt :transitions); :fn :args)
+;(defstruct transition :delay :start-txt :xtions); :fn :args)
 
-(defmethod queue-up :elf [e mtg-place]
-  (do
-    (display "Elf" (:id e) "figures it's time to see the fat old bastard...")
-    (handle [Exception (do
-			 (display "Elf" (:id e) "tried it's best.  Better luck next time.")
-			 (send-off e screw-off mtg-place))]
+(defn whip-slave [{:keys [type id task] :as state}]
+  (let [{:keys [start-txt fun args]} task]
+    (if start-txt
+      (display type id start-txt))
+    (if args
+      (do
+	;(display fun "..." args)
+	(apply fun state args))
+      (do
+	;(display fun "... no args")
+	(fun state)))))
+
+(def wait-patiently (struct task
+		      nil
+		      "wasting time in line..."
+		      identity
+		      nil))
+			    
+(defn get-in-line [state]
+  (handle [Exception e (do
+			 (display (:type state) (:id state)
+				  "tried it's best.  Better luck next time.")
+			 (assoc state :task screw-off))]
+	  (let [mtg-place (:mtg-place state)]
 	    (dosync
+	     ;(ensure mtg-place) not sure if this is needed...
 	     (let [place @mtg-place]
 	       (if (:full place)
 		 (throw (Exception. "No room"))
-		 (do
-		   (let [q (:queue place)
-			 newq (conj q e)
-			 cap (:capacity place)]
-		     (ensure mtg-place) 
-		     (alter mtg-place assoc :queue newq :full (< (count newq) cap))))))))
-    e))
+		 (let [q (:queue place)
+		       newq (conj q state)
+		       cap (:capacity place)]
+		   (alter mtg-place assoc :queue newq :full (< (count newq) cap))
+		   (assoc state :task wait-patiently))))))))
+
+(def queue-up (struct task
+		      nil
+		      "figures it's time to go see the fat old bastard..."
+		      get-in-line
+		      nil))
+
+(def screw-off (struct task
+		       screw-off-time
+		       "pretending to do something..."
+		       assoc
+		       [:task queue-up]))
 
 (defn main [elf-cap deer-cap]
-  (let [study (ref {:capacity elf-cap :full nil :queue [] :in []}); :validator valid-mtg-state?)
+  (let [study (ref {:capacity elf-cap :full nil :queue [] :in []})
 	sleigh (ref {:capacity elf-cap :full nil :queue [] :in []})
         elves (for [n (range 1 10)] 
-		(agent {:Type :elf
+		(agent {:type "Elf"
 			:id n
+			:task screw-off
 			:mtg-place study}))
 	deer (for [n (range 1 9)]
-	       (agent {:Type :deer
+	       (agent {:type "Deer"
 		       :id n
+		       :task screw-off
 		       :mtg-place sleigh}))
-	santa (agent {:active-place nil :study study :sleigh sleigh})]
-    (map deref elves)
-    (map (fn [a] (send-off a screw-off)) elves)
+	santa (agent {:type "Master"
+		      :active-place nil
+		      :task screw-off
+		      :study study
+		      :sleigh sleigh})]
+    ;(map deref elves)
+    ;(map (fn [a] (send-off a screw-off)) elves)
     [study sleigh elves deer santa]))
     ;(add-watch sleigh  )
     ;(add-watch study )
@@ -80,101 +105,29 @@
 ;;==============================================================
 
 (comment
-(defn queue-up [slave]
-  (let [mtg-place @(:mtg-place slave)]
-    (if (or (.contains (:in mtg-place) slave)
-	    (:full mtg-place))
-      nil
-      (send (:mtg-place slave) #(conj (:queue %) slave)))))
-
 (def study (ref {:capacity 3 :full nil :queue [] :in []}))
 
-(def elf1 (agent {:Type :elf
-		  :id 55}))
+(add-watch study :study-watch (fn [k r old new]
+				(let [{:keys [full capacity queue]} new
+				      qcnt (count queue)]
+				  (if full
+				    (cond
+				      (= capacity qcnt) (display "Newly full" "study queue")
+				      (= 0 qcnt) (display "Empty" "study queue")
+				      :true (display "Emptying the" "study queue"))))))
 
-(send elf1 screw-off study)
+(remove-watch study :study-watch)
+				  
+(def elf (agent {:type "Elf"
+		  :id 55
+		  :task screw-off
+		  :mtg-place study}))
 
-(defn enter [slave]
-  ())
-				       
-      
-(defn add-to-q [mtg-place]())
-
-	    
-(defn meet-the-master [slave]
-  (let [mtg-place @(:meeting-place slave)]
-    (if (or (.contains (:in mtg-place))
-	    (:full mtg-place))
-      nil
-      (dosync
-       (let [mtg-place @(:meeting-place slave)]
-	 (alter meet-ref conj slave)
-	 (if (= (:capacity slave) (count @q-ref))
-	   (alter slave-ref assoc :full :true)))))))
+(send elf whip-slave)
+(dosync (alter study assoc :queue [] :full nil))
 
 (defn slave-monitor [slave task]
   (agent {:slave slave :task task}))
 
-(defn beat-slave [{slave :slave task :task}]
-  (send slave task))
-
-(defn new-task [state task]
-  (assoc state :task task))
-
-(defn enter-mtg-place [slave]
-  ())
-  
-(defn workflow [grp task]
-  (do
-   (let [[ingate outgate] (join-grp grp)]
-     (do
-       (pass-gate ingate)
-       (task)
-       (pass-gate outgate)))))
-
-(defmacro check [pred tx]
-  (when-not pred (throw (RuntimeException. "Failed check!"))))
-
-(defn pass-gate [{rem :rem-capacity}]
-  (dosync
-   (while (zero? @rem)
-     ; put sleep here
-     (Thread/sleep 500)
-     )
-   (alter rem dec)))
-
-(defn operate-gate [{rem :rem-capacity} :as gate]
- (do
-   (dosync
-    (ref-set rem (:capacity gate)))
-   (dosync
-    (while (pos? @rem)
-      ;this is a blocking loop
-     (Thread/sleep 500)
-      ;may need to sleep or something here...
-      ))))
-
-(defn join-grp [{state :state} :as grp]
-  (dosync
-   (while (zero? (:openings @state))
-      ;this is a blocking loop
-     (Thread/sleep 500)
-      ;may need to sleep or something here...
-     )
-   (alter state #(assoc % :openings (dec (:openings %))))
-   (let [curstate @state]
-     (vector (:ingate state)(:outgate state)))))
-
-(defn await-grp [{state :state} :as grp]
-  (do
-   (while (pos? (:openings @state))
-      ;this is a blocking loop
-     (Thread/sleep 500)
-      ;may need to sleep or something here...
-     )
-   (let [n (:capacity grp)
-	 new-ingate (make-gate n)
-	 new-outgate (make-gate n)]
-     (ref-set state (make-group-state n new-ingate new-outgate))
-     (vector new-ingate new-outgate))))
 )
+
