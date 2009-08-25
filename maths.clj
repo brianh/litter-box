8e852be34d8a060f8172a0d16a18461e7a7bbf10
brianh <brian.h.work@gmail.com>
2009-08-25 16:26:31 -0600

    tweak

--- maths.clj
+++ maths.clj
@@ -1,5 +1,6 @@
-
 (defn a-to-n [a n] (rem (reduce * (repeat n a)) n))
 (defn naturals [n] (take n (iterate inc 1)))
 (defn hw [n] (map #(map (partial a-to-n %) (naturals n)) (naturals n)))
 (defn print-hw [n] (print (apply str (map (partial apply prn-str) (hw n)))))
+(defn print-row [n] (apply str (interleave n (repeat ","))))
+(defn print-table [n] (print (apply str (interleave (map print-row (hw n)) (repeat "\n")))))
