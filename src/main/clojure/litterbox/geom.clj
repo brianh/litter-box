b5400794202548bf17c115fdd26e1866a22dcb6d
bhalbert <bhalbert@psl.nmsu.edu>
2010-11-18 07:16:12 -0700

    Switched to D.

--- src/main/clojure/litterbox/geom.clj
+++ src/main/clojure/litterbox/geom.clj
@@ -4,8 +4,8 @@
   (load-file "/home/brian/code/clj/litterbox/src/main/clojure/litterbox/geom.clj")
   (in-ns 'litterbox.geom))
 
-(defrecord Point2d [^double x ^double y])
-(defrecord Point3d [^double x ^double y ^double z])
+(defrecord Point2D [^double x ^double y])
+(defrecord Point3D [^double x ^double y ^double z])
 
 (defprotocol Vector
   (scale [this s])
@@ -13,27 +13,25 @@
   (add [this v])
   (mag [this])
   (cross [this v])
-  (make-unit [this])
-  (rank [this]))
+  (make-unit [this]))
 
-(defrecord Vector3d [x y z])
+(defrecord Vector3D [x y z])
 
-(defrecord Vector2d [x y]
+(defrecord Vector2D [x y]
   Vector
   (scale [this s]
-	 (Vector2d. (* (:x this) s) (* (:y this) s)))
+	 (Vector2D. (* (:x this) s) (* (:y this) s)))
   (dot [this v]
        (* (:x this) (:x v)) (* (:y this) (:y v)))
   (add [this v]
-       (Vector2d. (+ (:x this) (:x v)) (+ (:y this) (:y v))))
+       (Vector2D. (+ (:x this) (:x v)) (+ (:y this) (:y v))))
   (mag [this]
        (Math/sqrt (reduce + (map #(* % %)
 				 (vals this)))))
   (cross [this v]
-	 (Vector3d. 0.0 0.0 (- (* (:x this)
+	 (Vector3D. 0.0 0.0 (- (* (:x this)
 				  (:y v))
 			       (* (:y this)
 				  (:x v)))))
   (make-unit [this]
-	     (scale this (/ 1 (mag this))))
-  (rank [this] 2))
+	     (scale this (/ 1 (mag this)))))
