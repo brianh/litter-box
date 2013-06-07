f22fb7c6f33e60f4d06afe901c8c407c7443203a
Brian Halbert <brian.halbert.1.0@gmail.com>
2013-06-06 23:18:48 -0400

    Update project
    * Move to 1.5.1
    * Starting to mess with datomic

--- project.clj
+++ project.clj
@@ -1,3 +1,8 @@
 (defproject litterbox "1.0.0-SNAPSHOT"
   :description "Place where mental dropping goes."
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/test.generative "0.3.0"]
+                 [com.datomic/datomic-free "0.8.3941"]
+                 [incanter/incanter-charts "1.3.0"]
+                 [incanter/incanter-pdf "1.3.0"]])
