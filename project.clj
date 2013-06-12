b7a5b1e79708c17f4631d7769a68a40f0b3f83d7
bhalbert <brian.h.work@gmail.com>
2013-06-11 21:12:53 -0400

    fixed desc

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject litterbox "1.0.0-SNAPSHOT"
-  :description "Place where mental dropping goes."
+  :description "Place where mental droppings go."
 
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/test.generative "0.3.0"]
