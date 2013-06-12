f14a83964eacb8ae4eb6af6d8db21932b857b2b0
bhalbert <brian.h.work@gmail.com>
2013-06-11 21:10:11 -0400

    Schema changes
    * Moved to data file
    * Fixed ns in core
    * Repl helpers for messing around

--- src/main/clojure/litterbox/sea/core.clj
+++ src/main/clojure/litterbox/sea/core.clj
@@ -1,10 +1,41 @@
-(ns 'litterbox.sea.core
-  (use [datomic.api :only [q db] :as d]))
+(ns litterbox.sea.core
+  (:use [datomic.api :as d]))
+
+(comment
 
 (def uri "datomic:mem://sea")
 
+(d/delete-database uri)
 (d/create-database uri)
 
 (def conn (d/connect uri))
 
-(def schema-attrs (q '[:find ?name :where [_ :db.install/attribute ?a] [?a :db/ident :entity]]))
+;(def schema-attrs (q '[:find ?name :where [_ :db.install/attribute ?a] [?a :db/ident :entity]]))
+
+(def sea-schema (read-string (slurp "/home/bhalbert/code/clj/litter-box/resources/sea-schema.edn")))
+
+(d/transact conn sea-schema)
+
+(defn new-entity [name]
+  [ {:db/id #db/id [:attributes]
+     :attribute/name name}])
+
+(def bob (new-entity "Bob"))
+
+(d/transact conn bob)
+
+(def named-entities-q '[:find ?e ?n :where [?e :attribute/name ?n]])
+
+(def positioned-entities-q
+  '[:find ?e ?p-x ?p-y
+    :where [?e :attribute.position/x ?p-x]
+    [?e :attribute.position/y ?p-y]])
+
+(def named-rs (d/q named-entities-q (d/db conn)))
+(def positioned-rs (d/q positioned-entities-q (d/db conn)))
+
+
+
+
+(defn set-attr [e & kvs]
+  [(apply assoc {:db/id (:db/id e)} kvs)]))
--- src/main/clojure/litterbox/sea/entities.clj
+++ src/main/clojure/litterbox/sea/entities.clj
@@ -1,11 +1,3 @@
-(ns litterbox.sea.entities
-  (use [datomic.api :only [q db] :as d]))
+(ns litterbox.sea.entities)
 
-
-(def entity-schema "[ {:db/id #db/id[:db.part/db]
-  :db/ident :entity
-  :db/valueType :db.type/long
-  :db/cardinality :db.cardinality/one
-  :db/doc \"An entity's id\"
-  :db.install/_attribute :db.part/db}]")
 
